bbb78fb38f11e56e83217a50382d163b4c48988b
Sruthi Baratam <sruthi.baratam20@outlook.com>
2026-07-17 12:15:29 +0530

    Move test file inside app folder to fix node_modules resolution

---
+++ app/test/server.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const { expect } = require('chai');
+const app = require('../server');
+
+describe('Hello World App', () => {
+  it('GET / should return 200 and greeting text', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).to.equal(200);
+    expect(res.text).to.include('Hello World');
+  });
+
+  it('GET /health should return status ok', async () => {
+    const res = await request(app).get('/health');
+    expect(res.status).to.equal(200);
+    expect(res.body.status).to.equal('ok');
+  });
+});
